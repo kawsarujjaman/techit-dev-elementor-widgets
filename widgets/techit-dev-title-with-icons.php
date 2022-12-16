f0aec4c8ca39f85cbb4c7ab064e656f4b9c5a39c
Kawsarujjaman <kaws01717@gmail.com>
2022-12-16 22:39:01 +0600

    second widget

--- widgets/techit-dev-title-with-icons.php
+++ widgets/techit-dev-title-with-icons.php
@@ -30,4 +30,45 @@ class techitdev_second_widgets extends \Elementor\Widget_Base {
         return 'https://techitdev.com';
     }   
    
+
+    // Content Tab start here
+
+    protected function register_controls()
+    {
+        $this-> start_controls_section(
+            'section_title',
+            [
+                'lebel'=> esc_html__( 'Title', 'techitdev' ),
+                'tab'=> \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+        $this-> add_control(
+            'title',
+            [
+                'label'=> esc_html__( 'Title', 'techitdev' ),
+                'type'=> \Elementor\Controls_Manager::TEXTAREA,
+                'default'=>  esc_html__( 'John Doe', 'techitdev' )
+            ]
+        );
+
+        $this->end_controls_section();
+
+
+    }
+
+
+    protected function render()
+    {
+        $settings= $this->get_settings_for_display();
+
+        ?>
+        <h3 class="card-title">
+          <?php echo $settings['title'];?>
+        </h3>
+        <p class="card__descriptions">
+          <!-- <?php echo $settings['description'];?> -->
+        </p>
+        <?php
+    }
 }
